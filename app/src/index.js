7ab71f3a15d3c9cb033c1c1cf88c2850c4311e89
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-06-20 01:12:28 +0100

    ...

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './script';
 import './index.css';
 import App from './App';
 
